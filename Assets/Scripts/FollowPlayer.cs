97d1418656394fed20a9a35bcada00c9446ddb09
darkhorse <managoy52@gmail.com>
2020-12-21 12:58:24 +0545

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;   
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // offset the camera to a position with respect to the player
+        transform.position = player.transform.position + offset;       
+    }
+}
